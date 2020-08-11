1ea37e270a959b7a470d4c046262f2023d895ebc
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-11 16:46:34 -0500

    Complete PayrollWithOvertime

---
+++ Walkthroughs/Payroll with Overtime/Payroll with Overtime/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Payroll_with_Overtime
+{
+    partial class payrollForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hoursPromptLabel = new System.Windows.Forms.Label();
+            this.payRatePromptLabel = new System.Windows.Forms.Label();
+            this.grossPayPromptLabel = new System.Windows.Forms.Label();
+            this.hoursWorkedTextBox = new System.Windows.Forms.TextBox();
+            this.hourlyRateTextBox = new System.Windows.Forms.TextBox();
+            this.grossPayLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // hoursPromptLabel
+            // 
+            this.hoursPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursPromptLabel.Location = new System.Drawing.Point(6, 22);
+            this.hoursPromptLabel.Name = "hoursPromptLabel";
+            this.hoursPromptLabel.Size = new System.Drawing.Size(256, 35);
+            this.hoursPromptLabel.TabIndex = 0;
+            this.hoursPromptLabel.Text = "Hours Worked : ";
+            this.hoursPromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // payRatePromptLabel
+            // 
+            this.payRatePromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payRatePromptLabel.Location = new System.Drawing.Point(10, 63);
+            this.payRatePromptLabel.Name = "payRatePromptLabel";
+            this.payRatePromptLabel.Size = new System.Drawing.Size(252, 47);
+            this.payRatePromptLabel.TabIndex = 1;
+            this.payRatePromptLabel.Text = "Hourly pay rate :";
+            this.payRatePromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // grossPayPromptLabel
+            // 
+            this.grossPayPromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grossPayPromptLabel.Location = new System.Drawing.Point(6, 110);
+            this.grossPayPromptLabel.Name = "grossPayPromptLabel";
+            this.grossPayPromptLabel.Size = new System.Drawing.Size(256, 42);
+            this.grossPayPromptLabel.TabIndex = 2;
+            this.grossPayPromptLabel.Text = "Gross pay :";
+            this.grossPayPromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // hoursWorkedTextBox
+            // 
+            this.hoursWorkedTextBox.Location = new System.Drawing.Point(289, 32);
+            this.hoursWorkedTextBox.Name = "hoursWorkedTextBox";
+            this.hoursWorkedTextBox.Size = new System.Drawing.Size(171, 20);
+            this.hoursWorkedTextBox.TabIndex = 3;
+            // 
+            // hourlyRateTextBox
+            // 
+            this.hourlyRateTextBox.Location = new System.Drawing.Point(289, 79);
+            this.hourlyRateTextBox.Name = "hourlyRateTextBox";
+            this.hourlyRateTextBox.Size = new System.Drawing.Size(171, 20);
+            this.hourlyRateTextBox.TabIndex = 5;
+            // 
+            // grossPayLabel
+            // 
+            this.grossPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grossPayLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grossPayLabel.Location = new System.Drawing.Point(289, 116);
+            this.grossPayLabel.Name = "grossPayLabel";
+            this.grossPayLabel.Size = new System.Drawing.Size(171, 31);
+            this.grossPayLabel.TabIndex = 6;
+            this.grossPayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(37, 252);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(124, 62);
+            this.calculateButton.TabIndex = 7;
+            this.calculateButton.Text = "Calculate Gross Pay";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(336, 252);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(124, 62);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(190, 252);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(124, 62);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // payrollForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 748);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.grossPayLabel);
+            this.Controls.Add(this.hourlyRateTextBox);
+            this.Controls.Add(this.hoursWorkedTextBox);
+            this.Controls.Add(this.grossPayPromptLabel);
+            this.Controls.Add(this.payRatePromptLabel);
+            this.Controls.Add(this.hoursPromptLabel);
+            this.Name = "payrollForm";
+            this.Text = "Payroll with Overtime";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hoursPromptLabel;
+        private System.Windows.Forms.Label payRatePromptLabel;
+        private System.Windows.Forms.Label grossPayPromptLabel;
+        private System.Windows.Forms.TextBox hoursWorkedTextBox;
+        private System.Windows.Forms.TextBox hourlyRateTextBox;
+        private System.Windows.Forms.Label grossPayLabel;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Walkthroughs/Payroll with Overtime/Payroll with Overtime/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Payroll_with_Overtime
+{
+    public partial class payrollForm : Form
+    {
+        public payrollForm()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            const decimal BASE_HOURS = 40m;
+            const decimal OT_MULTIPLIER = 1.5m;
+
+            try
+            {
+                decimal hoursWorked, hourlyPayRate, basePay;
+                decimal overtimeHours, overtimePay, grossPay;
+
+                hoursWorked = decimal.Parse(hoursWorkedTextBox.Text);
+                hourlyPayRate = decimal.Parse(hourlyRateTextBox.Text);
+
+                // Check if user did overtime
+                if(hoursWorked > BASE_HOURS)
+                {
+                    // Get overtime pay and base pay 
+                    basePay = hourlyPayRate * BASE_HOURS;
+                    overtimeHours = hoursWorked - BASE_HOURS;
+                    overtimePay = overtimeHours * hourlyPayRate * OT_MULTIPLIER;
+
+                    // Get gross pay
+                    grossPay = overtimePay + basePay;
+                }
+                else
+                {
+                    grossPay = hourlyPayRate * hoursWorked;
+                }
+
+                // Display the gross pay
+                grossPayLabel.Text = grossPay.ToString();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            // Clear the text in the text boxes
+            grossPayLabel.Text = "";
+            hoursWorkedTextBox.Text = "";
+            hourlyRateTextBox.Text = "";
+
+            // Refocus 
+            hoursWorkedTextBox.Focus();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the program
+            this.Close();
+        }
+    }
+}
