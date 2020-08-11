0bb5aba199ef35043fd6335b41b1942c9c46b036
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-10 23:22:12 -0500

    Complete SalePriceCalculator

---
+++ Walkthroughs/SalePriceCalculator/SalePriceCalculator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SalePriceCalculator
+{
+    partial class salesPriceCalculatorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.originalPricePromptLabel = new System.Windows.Forms.Label();
+            this.discPercentagePromptLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.salePriceLabel = new System.Windows.Forms.Label();
+            this.originalPriceTextBox = new System.Windows.Forms.TextBox();
+            this.discountPercentageTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // originalPricePromptLabel
+            // 
+            this.originalPricePromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.originalPricePromptLabel.Location = new System.Drawing.Point(12, 32);
+            this.originalPricePromptLabel.Name = "originalPricePromptLabel";
+            this.originalPricePromptLabel.Size = new System.Drawing.Size(277, 29);
+            this.originalPricePromptLabel.TabIndex = 0;
+            this.originalPricePromptLabel.Text = "Enter the item\'s original price:";
+            this.originalPricePromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // discPercentagePromptLabel
+            // 
+            this.discPercentagePromptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discPercentagePromptLabel.Location = new System.Drawing.Point(12, 82);
+            this.discPercentagePromptLabel.Name = "discPercentagePromptLabel";
+            this.discPercentagePromptLabel.Size = new System.Drawing.Size(277, 29);
+            this.discPercentagePromptLabel.TabIndex = 1;
+            this.discPercentagePromptLabel.Text = "Enter the discount percentage:";
+            this.discPercentagePromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sales Price:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // salePriceLabel
+            // 
+            this.salePriceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.salePriceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.salePriceLabel.Location = new System.Drawing.Point(314, 167);
+            this.salePriceLabel.Name = "salePriceLabel";
+            this.salePriceLabel.Size = new System.Drawing.Size(177, 29);
+            this.salePriceLabel.TabIndex = 3;
+            this.salePriceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // originalPriceTextBox
+            // 
+            this.originalPriceTextBox.Location = new System.Drawing.Point(309, 36);
+            this.originalPriceTextBox.Name = "originalPriceTextBox";
+            this.originalPriceTextBox.Size = new System.Drawing.Size(181, 20);
+            this.originalPriceTextBox.TabIndex = 4;
+            // 
+            // discountPercentageTextBox
+            // 
+            this.discountPercentageTextBox.Location = new System.Drawing.Point(309, 89);
+            this.discountPercentageTextBox.Name = "discountPercentageTextBox";
+            this.discountPercentageTextBox.Size = new System.Drawing.Size(181, 20);
+            this.discountPercentageTextBox.TabIndex = 5;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(89, 305);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(151, 60);
+            this.calculateButton.TabIndex = 6;
+            this.calculateButton.Text = "Calculate Sales Price";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(309, 305);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(151, 60);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // salesPriceCalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 512);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.discountPercentageTextBox);
+            this.Controls.Add(this.originalPriceTextBox);
+            this.Controls.Add(this.salePriceLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.discPercentagePromptLabel);
+            this.Controls.Add(this.originalPricePromptLabel);
+            this.Name = "salesPriceCalculatorForm";
+            this.Text = "Sales Price Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label originalPricePromptLabel;
+        private System.Windows.Forms.Label discPercentagePromptLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label salePriceLabel;
+        private System.Windows.Forms.TextBox originalPriceTextBox;
+        private System.Windows.Forms.TextBox discountPercentageTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Walkthroughs/SalePriceCalculator/SalePriceCalculator/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalePriceCalculator
+{
+    public partial class salesPriceCalculatorForm : Form
+    {
+        public salesPriceCalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // Declare varaibales to hold specific vlaues
+            double percentage, originalPrice, salesPrice, discount;
+
+            // Get values from input
+            originalPrice = double.Parse(originalPriceTextBox.Text);
+            salesPrice = originalPrice;
+            percentage = double.Parse(discountPercentageTextBox.Text) / 100;
+
+            // Perform the MF arithmatic operations
+            discount = originalPrice * percentage;
+            salesPrice -= discount;
+
+            salePriceLabel.Text = salesPrice.ToString("c");
+        }
+    }
+}
