52fbf3a9a5c318485f37c08ebfd9ba5f18236f8b
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-12 17:28:14 -0500

    Complete RandomFileReader

---
+++ Chapter Problems/Chapter05/RandomNumberFileReader/RandomNumberFileReader/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace RandomNumberFileReader
+{
+    partial class randomForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numberPromptLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.runNumbersButton = new System.Windows.Forms.Button();
+            this.getNumbersButton = new System.Windows.Forms.Button();
+            this.numbersListBox = new System.Windows.Forms.ListBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // numberPromptLabel
+            // 
+            this.numberPromptLabel.Location = new System.Drawing.Point(12, 25);
+            this.numberPromptLabel.Name = "numberPromptLabel";
+            this.numberPromptLabel.Size = new System.Drawing.Size(146, 18);
+            this.numberPromptLabel.TabIndex = 0;
+            this.numberPromptLabel.Text = "How many random number : ";
+            this.numberPromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(164, 25);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(108, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // runNumbersButton
+            // 
+            this.runNumbersButton.Location = new System.Drawing.Point(70, 63);
+            this.runNumbersButton.Name = "runNumbersButton";
+            this.runNumbersButton.Size = new System.Drawing.Size(134, 59);
+            this.runNumbersButton.TabIndex = 2;
+            this.runNumbersButton.Text = "Genereate Random Number to File";
+            this.runNumbersButton.UseVisualStyleBackColor = true;
+            this.runNumbersButton.Click += new System.EventHandler(this.runNumbersButton_Click);
+            // 
+            // getNumbersButton
+            // 
+            this.getNumbersButton.Location = new System.Drawing.Point(70, 144);
+            this.getNumbersButton.Name = "getNumbersButton";
+            this.getNumbersButton.Size = new System.Drawing.Size(134, 59);
+            this.getNumbersButton.TabIndex = 3;
+            this.getNumbersButton.Text = "Return Number from File";
+            this.getNumbersButton.UseVisualStyleBackColor = true;
+            this.getNumbersButton.Click += new System.EventHandler(this.getNumbersButton_Click);
+            // 
+            // numbersListBox
+            // 
+            this.numbersListBox.FormattingEnabled = true;
+            this.numbersListBox.Location = new System.Drawing.Point(60, 222);
+            this.numbersListBox.Name = "numbersListBox";
+            this.numbersListBox.Size = new System.Drawing.Size(164, 121);
+            this.numbersListBox.TabIndex = 4;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(70, 380);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(134, 59);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            // 
+            // randomForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 451);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.numbersListBox);
+            this.Controls.Add(this.getNumbersButton);
+            this.Controls.Add(this.runNumbersButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.numberPromptLabel);
+            this.Name = "randomForm";
+            this.Text = "Random Number";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label numberPromptLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button runNumbersButton;
+        private System.Windows.Forms.Button getNumbersButton;
+        private System.Windows.Forms.ListBox numbersListBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ Chapter Problems/Chapter05/RandomNumberFileReader/RandomNumberFileReader/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RandomNumberFileReader
+{
+    public partial class randomForm : Form
+    {
+        // Open file 
+        StreamWriter outFile;
+        StreamReader inFile;
+
+        public randomForm()
+        {
+            InitializeComponent();
+        }
+
+        private void runNumbersButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Variables declaration
+                int counter;
+
+                // Create rabdom object 
+                Random rand = new Random();
+
+                if(int.TryParse(numberTextBox.Text, out counter))
+                {
+                    if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        outFile = File.AppendText(saveFileDialog.FileName);
+                        
+                        for(int i = 0; i < counter; i++)
+                        {
+                            outFile.WriteLine(rand.Next(1, 100));
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Error please eneter correct amount of random numbers");
+                }
+                outFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void getNumbersButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                    inFile = File.OpenText(saveFileDialog.FileName);
+                    while(!inFile.EndOfStream)
+                    {
+                        numbersListBox.Items.Add(inFile.ReadLine());
+                    }
+                inFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the program
+            this.Close();
+        }
+    }
+}
