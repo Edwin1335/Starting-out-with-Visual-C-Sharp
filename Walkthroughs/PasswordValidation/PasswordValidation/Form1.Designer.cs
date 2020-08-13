91ade86220d50ff4d0f792f588811249054e94f6
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-13 15:05:18 -0500

    Complete PasswordValidation

---
+++ Walkthroughs/PasswordValidation/PasswordValidation/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PasswordValidation
+{
+    partial class passwordLabel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.passwordPromptLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // passwordPromptLabel
+            // 
+            this.passwordPromptLabel.Location = new System.Drawing.Point(12, 40);
+            this.passwordPromptLabel.Name = "passwordPromptLabel";
+            this.passwordPromptLabel.Size = new System.Drawing.Size(105, 27);
+            this.passwordPromptLabel.TabIndex = 0;
+            this.passwordPromptLabel.Text = "Enter Password ";
+            this.passwordPromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(134, 44);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(146, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // PasswordButton
+            // 
+            this.PasswordButton.Location = new System.Drawing.Point(55, 85);
+            this.PasswordButton.Name = "PasswordButton";
+            this.PasswordButton.Size = new System.Drawing.Size(89, 41);
+            this.PasswordButton.TabIndex = 2;
+            this.PasswordButton.Text = "Check Password";
+            this.PasswordButton.UseVisualStyleBackColor = true;
+            this.PasswordButton.Click += new System.EventHandler(this.PasswordButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(173, 85);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(89, 41);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // passwordLabel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 138);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.PasswordButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordPromptLabel);
+            this.Name = "passwordLabel";
+            this.Text = "Password Validation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label passwordPromptLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button PasswordButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Walkthroughs/PasswordValidation/PasswordValidation/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordValidation
+{
+    public partial class passwordLabel : Form
+    {
+        public passwordLabel()
+        {
+            InitializeComponent();
+        }
+
+        private bool ValidatePassword(ref string pass)
+        {
+            bool passNumber = false;
+            bool passUpper = false;
+            bool passLower = false;
+
+            // Check validation for "Must include at least one number"
+            foreach(char c in pass)
+            {
+                if (char.IsDigit(c))
+                {
+                    passNumber = true;
+                    break;
+                }
+            }
+
+            // Check validation for "Must include at least one lowercase letter"
+            foreach (char c in pass)
+            {
+                if (char.IsLower(c))
+                {
+                    passLower = true;
+                    break;
+                }
+            }
+
+            // Check validation for "Must include at least one uppercase letter"
+            foreach (char c in pass)
+            {
+                if (char.IsUpper(c))
+                {
+                    passUpper = true;
+                    break;
+                }
+            }
+
+            // Check final logical validations
+            if (pass == "")
+            {
+                MessageBox.Show("Password cannot be empty");
+                return false;
+            }
+            if(pass.Length < 8)
+            {
+                MessageBox.Show("Password myst be at least 8 characters long");
+                return false;
+            }
+            if (!passUpper)
+            {
+                MessageBox.Show("Password must contain an upper case letter");
+                return false;
+            }
+            else if(!passLower)
+            {
+                MessageBox.Show("Password must contain an lower case letter");
+                return false;
+            }
+            else if (!passNumber)
+            {
+                MessageBox.Show("Password must contain a number");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void PasswordButton_Click(object sender, EventArgs e)
+        {
+            string password = passwordTextBox.Text;
+
+            // Check password validation
+            try
+            {
+                if(ValidatePassword(ref password))
+                {
+                    MessageBox.Show("CORRECT PASSWORD !!!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Exit the program
+            this.Close();
+        }
+    }
+}
