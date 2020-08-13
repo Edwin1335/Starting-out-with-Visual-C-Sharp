37e0cd68190265bd39de50b054bcc10f6b89947d
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-13 17:38:30 -0500

    Complete CarList

---
+++ Walkthroughs/CarsList/CarsList/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace CarsList
+{
+    partial class carListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carInfoGroupBox = new System.Windows.Forms.GroupBox();
+            this.makePromptLabel = new System.Windows.Forms.Label();
+            this.yearPromptLabel = new System.Windows.Forms.Label();
+            this.milePromptLabel = new System.Windows.Forms.Label();
+            this.mileTextBox = new System.Windows.Forms.TextBox();
+            this.yearTexBox = new System.Windows.Forms.TextBox();
+            this.mileageTextBox = new System.Windows.Forms.TextBox();
+            this.AddCarButton = new System.Windows.Forms.Button();
+            this.carListBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.carInfoGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // carInfoGroupBox
+            // 
+            this.carInfoGroupBox.Controls.Add(this.mileageTextBox);
+            this.carInfoGroupBox.Controls.Add(this.yearTexBox);
+            this.carInfoGroupBox.Controls.Add(this.mileTextBox);
+            this.carInfoGroupBox.Controls.Add(this.milePromptLabel);
+            this.carInfoGroupBox.Controls.Add(this.yearPromptLabel);
+            this.carInfoGroupBox.Controls.Add(this.makePromptLabel);
+            this.carInfoGroupBox.Location = new System.Drawing.Point(45, 44);
+            this.carInfoGroupBox.Name = "carInfoGroupBox";
+            this.carInfoGroupBox.Size = new System.Drawing.Size(368, 229);
+            this.carInfoGroupBox.TabIndex = 0;
+            this.carInfoGroupBox.TabStop = false;
+            this.carInfoGroupBox.Text = "Car Infomation";
+            // 
+            // makePromptLabel
+            // 
+            this.makePromptLabel.Location = new System.Drawing.Point(6, 52);
+            this.makePromptLabel.Name = "makePromptLabel";
+            this.makePromptLabel.Size = new System.Drawing.Size(111, 49);
+            this.makePromptLabel.TabIndex = 0;
+            this.makePromptLabel.Text = "Make";
+            this.makePromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // yearPromptLabel
+            // 
+            this.yearPromptLabel.Location = new System.Drawing.Point(6, 101);
+            this.yearPromptLabel.Name = "yearPromptLabel";
+            this.yearPromptLabel.Size = new System.Drawing.Size(111, 49);
+            this.yearPromptLabel.TabIndex = 1;
+            this.yearPromptLabel.Text = "Year";
+            this.yearPromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // milePromptLabel
+            // 
+            this.milePromptLabel.Location = new System.Drawing.Point(6, 150);
+            this.milePromptLabel.Name = "milePromptLabel";
+            this.milePromptLabel.Size = new System.Drawing.Size(111, 49);
+            this.milePromptLabel.TabIndex = 2;
+            this.milePromptLabel.Text = "Mileage";
+            this.milePromptLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // mileTextBox
+            // 
+            this.mileTextBox.Location = new System.Drawing.Point(151, 64);
+            this.mileTextBox.Name = "mileTextBox";
+            this.mileTextBox.Size = new System.Drawing.Size(192, 31);
+            this.mileTextBox.TabIndex = 3;
+            // 
+            // yearTexBox
+            // 
+            this.yearTexBox.Location = new System.Drawing.Point(151, 110);
+            this.yearTexBox.Name = "yearTexBox";
+            this.yearTexBox.Size = new System.Drawing.Size(192, 31);
+            this.yearTexBox.TabIndex = 4;
+            // 
+            // mileageTextBox
+            // 
+            this.mileageTextBox.Location = new System.Drawing.Point(151, 159);
+            this.mileageTextBox.Name = "mileageTextBox";
+            this.mileageTextBox.Size = new System.Drawing.Size(192, 31);
+            this.mileageTextBox.TabIndex = 5;
+            // 
+            // AddCarButton
+            // 
+            this.AddCarButton.Location = new System.Drawing.Point(126, 308);
+            this.AddCarButton.Name = "AddCarButton";
+            this.AddCarButton.Size = new System.Drawing.Size(157, 65);
+            this.AddCarButton.TabIndex = 1;
+            this.AddCarButton.Text = "Add Car to List";
+            this.AddCarButton.UseVisualStyleBackColor = true;
+            this.AddCarButton.Click += new System.EventHandler(this.AddCarButton_Click);
+            // 
+            // carListBox1
+            // 
+            this.carListBox1.FormattingEnabled = true;
+            this.carListBox1.ItemHeight = 25;
+            this.carListBox1.Location = new System.Drawing.Point(439, 44);
+            this.carListBox1.Name = "carListBox1";
+            this.carListBox1.Size = new System.Drawing.Size(325, 229);
+            this.carListBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(503, 308);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 65);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Display Car List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // carListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(829, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.carListBox1);
+            this.Controls.Add(this.AddCarButton);
+            this.Controls.Add(this.carInfoGroupBox);
+            this.Name = "carListForm";
+            this.Text = "Car List";
+            this.carInfoGroupBox.ResumeLayout(false);
+            this.carInfoGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox carInfoGroupBox;
+        private System.Windows.Forms.TextBox mileageTextBox;
+        private System.Windows.Forms.TextBox yearTexBox;
+        private System.Windows.Forms.TextBox mileTextBox;
+        private System.Windows.Forms.Label milePromptLabel;
+        private System.Windows.Forms.Label yearPromptLabel;
+        private System.Windows.Forms.Label makePromptLabel;
+        private System.Windows.Forms.Button AddCarButton;
+        private System.Windows.Forms.ListBox carListBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Walkthroughs/CarsList/CarsList/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarsList
+{
+    struct Car
+    {
+        public string name;
+        public int year;
+        public double miealage;
+    }
+
+    public partial class carListForm : Form
+    {
+        private List<Car> carList = new List<Car>();
+
+        public carListForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool GetCarInfo(ref string make, ref int year, ref double mileage)
+        {
+            if(mileTextBox.Text == "" || yearTexBox.Text == "" || mileageTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter values for make year and mileage");
+                return false;
+            }
+            else
+            {
+                make = mileTextBox.Text;
+
+                if(!int.TryParse(yearTexBox.Text, out year) && year > 0 && year < 2050)
+                {
+                    MessageBox.Show("Year must be an integer");
+                    return false;
+                }
+                if(!double.TryParse(mileageTextBox.Text, out mileage) && mileage > 0)
+                {
+                    MessageBox.Show("Year must be an double");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void AddCarButton_Click(object sender, EventArgs e)
+        {
+            string make = "";
+            int year = -1;
+            double mileage = -1;
+            
+            // Insert new car into list 
+            if(GetCarInfo(ref make,ref year,ref mileage))
+            {
+                Car newCar;
+                newCar.name = make;
+                newCar.year = year;
+                newCar.miealage = mileage;
+
+                carList.Add(newCar);
+
+                // Clear all the values
+                mileTextBox.Text = "";
+                mileageTextBox.Text = "";
+                yearTexBox.Text = "";
+
+                // Refocus
+                mileTextBox.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach(Car c in carList)
+            {
+                carListBox1.Items.Add(c.name + " " + c.year + ", " + c.year);
+            }
+        }
+    }
+}
