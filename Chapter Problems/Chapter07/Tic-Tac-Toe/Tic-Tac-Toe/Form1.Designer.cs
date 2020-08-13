f3ca6bd58f72e731a978d8213cec8b95da5672c6
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-13 02:24:33 -0500

    Complete TicTacToe

---
+++ Chapter Problems/Chapter07/Tic-Tac-Toe/Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class ticTacToeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.topLeftlabel = new System.Windows.Forms.Label();
+            this.topCenterLabel = new System.Windows.Forms.Label();
+            this.topRightLabel = new System.Windows.Forms.Label();
+            this.centerLeftLabel = new System.Windows.Forms.Label();
+            this.centerCenterLabel = new System.Windows.Forms.Label();
+            this.centerRightLabel = new System.Windows.Forms.Label();
+            this.bottomLeftLabel = new System.Windows.Forms.Label();
+            this.bottomventerLabel = new System.Windows.Forms.Label();
+            this.bottomRightLabel = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLeftlabel
+            // 
+            this.topLeftlabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topLeftlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topLeftlabel.Location = new System.Drawing.Point(34, 37);
+            this.topLeftlabel.Name = "topLeftlabel";
+            this.topLeftlabel.Size = new System.Drawing.Size(57, 58);
+            this.topLeftlabel.TabIndex = 0;
+            this.topLeftlabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topCenterLabel
+            // 
+            this.topCenterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topCenterLabel.Location = new System.Drawing.Point(113, 37);
+            this.topCenterLabel.Name = "topCenterLabel";
+            this.topCenterLabel.Size = new System.Drawing.Size(57, 58);
+            this.topCenterLabel.TabIndex = 1;
+            this.topCenterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // topRightLabel
+            // 
+            this.topRightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.topRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.topRightLabel.Location = new System.Drawing.Point(192, 37);
+            this.topRightLabel.Name = "topRightLabel";
+            this.topRightLabel.Size = new System.Drawing.Size(57, 58);
+            this.topRightLabel.TabIndex = 2;
+            this.topRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // centerLeftLabel
+            // 
+            this.centerLeftLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerLeftLabel.Location = new System.Drawing.Point(34, 113);
+            this.centerLeftLabel.Name = "centerLeftLabel";
+            this.centerLeftLabel.Size = new System.Drawing.Size(57, 58);
+            this.centerLeftLabel.TabIndex = 3;
+            this.centerLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // centerCenterLabel
+            // 
+            this.centerCenterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerCenterLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerCenterLabel.Location = new System.Drawing.Point(113, 113);
+            this.centerCenterLabel.Name = "centerCenterLabel";
+            this.centerCenterLabel.Size = new System.Drawing.Size(57, 58);
+            this.centerCenterLabel.TabIndex = 4;
+            this.centerCenterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // centerRightLabel
+            // 
+            this.centerRightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.centerRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.centerRightLabel.Location = new System.Drawing.Point(192, 113);
+            this.centerRightLabel.Name = "centerRightLabel";
+            this.centerRightLabel.Size = new System.Drawing.Size(57, 58);
+            this.centerRightLabel.TabIndex = 5;
+            this.centerRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomLeftLabel
+            // 
+            this.bottomLeftLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomLeftLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomLeftLabel.Location = new System.Drawing.Point(34, 187);
+            this.bottomLeftLabel.Name = "bottomLeftLabel";
+            this.bottomLeftLabel.Size = new System.Drawing.Size(57, 58);
+            this.bottomLeftLabel.TabIndex = 6;
+            this.bottomLeftLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomventerLabel
+            // 
+            this.bottomventerLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomventerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomventerLabel.Location = new System.Drawing.Point(113, 187);
+            this.bottomventerLabel.Name = "bottomventerLabel";
+            this.bottomventerLabel.Size = new System.Drawing.Size(57, 58);
+            this.bottomventerLabel.TabIndex = 7;
+            this.bottomventerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottomRightLabel
+            // 
+            this.bottomRightLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bottomRightLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottomRightLabel.Location = new System.Drawing.Point(192, 187);
+            this.bottomRightLabel.Name = "bottomRightLabel";
+            this.bottomRightLabel.Size = new System.Drawing.Size(57, 58);
+            this.bottomRightLabel.TabIndex = 8;
+            this.bottomRightLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(34, 273);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(73, 41);
+            this.playButton.TabIndex = 9;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(187, 273);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(62, 41);
+            this.exitButton.TabIndex = 10;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(113, 273);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(68, 41);
+            this.clearButton.TabIndex = 11;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // ticTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 334);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.bottomRightLabel);
+            this.Controls.Add(this.bottomventerLabel);
+            this.Controls.Add(this.bottomLeftLabel);
+            this.Controls.Add(this.centerRightLabel);
+            this.Controls.Add(this.centerCenterLabel);
+            this.Controls.Add(this.centerLeftLabel);
+            this.Controls.Add(this.topRightLabel);
+            this.Controls.Add(this.topCenterLabel);
+            this.Controls.Add(this.topLeftlabel);
+            this.Name = "ticTacToeForm";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label topLeftlabel;
+        private System.Windows.Forms.Label topCenterLabel;
+        private System.Windows.Forms.Label topRightLabel;
+        private System.Windows.Forms.Label centerLeftLabel;
+        private System.Windows.Forms.Label centerCenterLabel;
+        private System.Windows.Forms.Label centerRightLabel;
+        private System.Windows.Forms.Label bottomLeftLabel;
+        private System.Windows.Forms.Label bottomventerLabel;
+        private System.Windows.Forms.Label bottomRightLabel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Chapter Problems/Chapter07/Tic-Tac-Toe/Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class ticTacToeForm : Form
+    {
+        public ticTacToeForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Display(char[,] myArray)
+        {
+            topLeftlabel.Text = myArray[0,0].ToString();
+            topCenterLabel.Text = myArray[0, 1].ToString();
+            topRightLabel.Text = myArray[0, 2].ToString();
+            centerLeftLabel.Text = myArray[1, 0].ToString();
+            centerCenterLabel.Text = myArray[1, 1].ToString();
+            centerRightLabel.Text = myArray[1, 2].ToString();
+            bottomLeftLabel.Text = myArray[2, 0].ToString();
+            bottomventerLabel.Text = myArray[2, 1].ToString();
+            bottomRightLabel.Text = myArray[2, 2].ToString();
+        }
+
+        public void DisplayWinner(char[,] myArray)
+        {
+            for(int i = 0; i < myArray.GetLength(0); i++)
+            {
+                if(myArray[i, 0] == myArray[i,1] && myArray[i, 2] == myArray[i,1])
+                {
+                    MessageBox.Show("Player " + myArray[i, 0].ToString() + " has WON");
+                    return;
+                }
+                if(myArray[0, i] == myArray[1, i] && myArray[2, i] == myArray[1, i])
+                {
+                    MessageBox.Show("Player " + myArray[0, i].ToString() + " has WON");
+                    return;
+                }
+            }
+
+            // Check diagnal win
+            if(myArray[0, 0] == myArray[1, 1] && myArray[1, 1] == myArray[2, 2])
+            {
+                MessageBox.Show("Player " + myArray[0, 0].ToString() + " has WON");
+                return;
+            }
+            if (myArray[0, 2] == myArray[1, 1] && myArray[1, 1] == myArray[2, 0])
+            {
+                MessageBox.Show("Player " + myArray[0, 2].ToString() + " has WON");
+                return;
+            }
+
+            MessageBox.Show("Its a tie!!");
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            const int COL = 3;
+            const int ROW = 3;
+            Random rand = new Random();
+
+            char[,] ticChar = new char[ROW, COL];
+
+            for (int i = 0; i < ROW; i++)
+            {
+                for(int j = 0; j < COL; j++)
+                {
+                    if (rand.Next(1, 3) == 1)
+                    {
+                        ticChar[i, j] = 'X';
+                    }
+                    else
+                    {
+                        ticChar[i, j] = 'O';
+                    }
+                }
+            }
+
+            Display(ticChar);
+            DisplayWinner(ticChar);
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            topLeftlabel.Text = "";
+            topCenterLabel.Text = "";
+            topRightLabel.Text = "";
+            centerLeftLabel.Text = "";
+            centerCenterLabel.Text = "";
+            centerRightLabel.Text = "";
+            bottomLeftLabel.Text = "";
+            bottomventerLabel.Text = "";
+            bottomRightLabel.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
