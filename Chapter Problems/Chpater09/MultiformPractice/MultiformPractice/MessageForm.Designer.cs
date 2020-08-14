45d20173f7694e85e11109e8e6d7b3081fb39850
Edwin Campuzano <campuzano.edwin1995@gmail.com>
2020-08-14 16:47:37 -0500

    Completed MiltiformPractice

---
+++ Chapter Problems/Chpater09/MultiformPractice/MultiformPractice/MessageForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MultiformPractice
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageLabel = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageLabel.Location = new System.Drawing.Point(58, 43);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(121, 16);
+            this.messageLabel.TabIndex = 0;
+            this.messageLabel.Text = "Have a great day !!";
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(78, 78);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(87, 27);
+            this.Close.TabIndex = 1;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // MessageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 134);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.messageLabel);
+            this.Name = "MessageForm";
+            this.Text = "MessageForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label messageLabel;
+        private System.Windows.Forms.Button Close;
+    }
+}
